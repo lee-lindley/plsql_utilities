63a694355ad8c3df978d646a49c71e5835491cf0
Lee Lindley <lee.lindley@gmail.com>
2021-08-29 16:55:35 -0400

    remove exit failure from sqlplus install script

--- app_dbms_sql/install_app_dbms_sql.sql
+++ app_dbms_sql/install_app_dbms_sql.sql
@@ -3,7 +3,7 @@ prompt OK for these to fail for type not exists
 DROP TYPE app_dbms_sql_str_udt;
 DROP TYPE app_dbms_sql_udt;
 prompt OK if failed for type not exists
-whenever sqlerror exit failure
+--whenever sqlerror exit failure
 --
 prompt beginning app_dbms_sql_udt.tps
 @&&subdir/app_dbms_sql_udt.tps
@@ -15,3 +15,5 @@ prompt beginning app_dbms_sql_str_udt.tps
 prompt beginning app_dbms_sql_str_udt.tpb
 @&&subdir/app_dbms_sql_str_udt.tpb
 prompt deployment of app_dbms_sql_udt, app_dbms_sql_str_udt types is complete
+prompt if the deployment reported that it could not drop or replace the types, then
+prompt you may need to drop any dependent types first
